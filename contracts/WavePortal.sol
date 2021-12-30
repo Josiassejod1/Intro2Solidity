cd919bc0a5de7fc638874426c559dfb16443e1b8
Dalvin <dalvin.josias@gmail.com>
2021-12-30 02:05:18 -0800

    add cooldown

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,6 +8,10 @@ import "hardhat/console.sol";
 contract WavePortal {
     uint256 totalWaves;
 
+    uint256 private seed;
+
+    mapping(address => uint256) public lastWavedAt;
+
     struct Wave {
         address waver;
         string message;
@@ -16,6 +20,7 @@ contract WavePortal {
 
     constructor() payable {
         console.log("Yo, yo I am a contract");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
 
@@ -26,25 +31,41 @@ contract WavePortal {
    
 
     function wave(string memory _message) public {
+
+        require(
+            lastWavedAt[msg.sender] + 1 minutes < block.timestamp,
+            "Wait 1m"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
+
         totalWaves += 1;
         
         console.log("%s has waved w/ message!", msg.sender, _message);
 
-         emit NewWave(msg.sender, block.timestamp, _message);
-        waves.push(Wave(msg.sender, _message, block.timestamp));
-        
-        uint256 prizeAmount = 0.0001 ether;
-        
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money"
-        );
+         waves.push(Wave(msg.sender, _message, block.timestamp));   
 
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdrawal funds");
+         seed = (block.difficulty + block.timestamp + seed) % 100;
+         console.log("Random # generated: %d", seed);
 
-       
-    }
+         if (seed <= 50) {
+              
+            uint256 prizeAmount = 0.0001 ether;
+        
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money"
+            );
+
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdrawal funds");
+            
+            }
+
+            emit NewWave(msg.sender, block.timestamp, _message);
+           
+        }
 
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves);
--- scripts/run.js
+++ scripts/run.js
@@ -7,6 +7,9 @@ const main = async () => {
         value: hre.ethers.utils.parseEther("0.1"),
     });
 
+    await waveContract.deployed();
+    console.log("Contract deployed to:", waveContract.address);
+
     let contractBalance = await hre.ethers.provider.getBalance(
         waveContract.address
     );
@@ -16,15 +19,20 @@ const main = async () => {
         hre.ethers.utils.formatEther(contractBalance),
     );
 
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
+
 
     // let waveCount;
     // waveCount = await waveContract.getTotalWaves();
     // console.log(waveCount.toNumber());
 
-    let waveTxn = await waveContract.wave("YERRRRR");
+    // let waveTxn = await waveContract.wave("YERRRRR");
+    // await waveTxn.wait();
+
+    const waveTxn = await waveContract.wave("This is wave #1");
     await waveTxn.wait();
+  
+    const waveTxn2 = await waveContract.wave("This is wave #2");
+    await waveTxn2.wait();
 
     
     contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
@@ -39,8 +47,8 @@ const main = async () => {
     // waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
     // await waveTxn.wait();
 
-    // let allWaves = await waveContract.getAllWaves();
-    // console.log(allWaves);
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
 
     // waveCount = await waveContract.getTotalWaves();
     // console.log(waveCount.toNumber());
